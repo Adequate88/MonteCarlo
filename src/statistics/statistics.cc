a75965a7288e11a71d3d9c827bf055da20ea246d
azbenoit <64805822+azbenoit@users.noreply.github.com>
2024-11-26 17:53:23 +0100

    Statistics Class Header

---
+++ src/statistics/statistics.cc
@@ -0,0 +1,10 @@
+//
+// Created by alixb1908 on 26/11/24.
+//
+#include "statistics.hh"
+
+
+
+
+
+
---
+++ src/statistics/statistics.hh
@@ -0,0 +1,42 @@
+#ifndef STATISTICS_HH
+#define STATISTICS_HH
+
+#include <vector>
+#include <functional>
+#include "abstract_sampler.hh"
+
+class Statistics {
+public:
+    // Constructor that accepts a sampler object
+    Statistics(const AbstractSampler& sampler, int num_samples);
+
+    // Standard Destructor
+    ~Statistics() = default;
+
+    // Function to set data (can be used to set the random samples)
+    void set_data(const std::vector<double>& samples);
+
+    // Function to generate and add N new samples
+    void gen_N_new_samples(int N);
+
+    // Expectation value for an arbitrary function f
+    double expectation(const std::function<double(double)>& f = [](double x) { return x; }) const;
+
+    // Variance for an arbitrary function f
+    double variance(const std::function<double(double)>& f = [](double x) { return x; }) const;
+
+    // Generalized function to calculate any power moment for an arbitrary function f
+    double moment(int power, const std::function<double(double)>& f = [](double x) { return x; }) const;
+
+    // Function to calculate central moments
+    double central_moment(int power, const std::function<double(double)>& f = [](double x) { return x; }) const;
+
+    // Function to clear data
+    void clear_data();
+
+private:
+    std::vector<double> data;
+    int N;
+};
+
+#endif // STATISTICS_HH
